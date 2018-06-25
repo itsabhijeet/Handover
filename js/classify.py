4ab9a21c1e7decc0fe2384a98b55e0ae8272ad5a
Abhijeet Kumar <25275343+itsabhijeet@users.noreply.github.com>
2018-06-26 01:03:22 +0530

    Update classify.py

--- js/classify.py
+++ js/classify.py
@@ -164,8 +164,8 @@ def sendemail(email):
     
     app.config['MAIL_SERVER']='smtp.gmail.com'
     app.config['MAIL_PORT'] = 465
-    app.config['MAIL_USERNAME'] = 'handoverabhi@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'handover'
+    app.config['MAIL_USERNAME'] = '' #email
+    app.config['MAIL_PASSWORD'] = '' #password
     app.config['MAIL_USE_TLS'] = False
     app.config['MAIL_USE_SSL'] = True
     mail = Mail(app)
