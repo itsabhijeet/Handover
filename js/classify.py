20b57dd55d0fc3dee351021cc5b09b6da40aae37
abhilearner <abhijeetown540@outlook.com>
2018-06-28 09:16:35 +0530

    Added requirement.txt

--- js/classify.py
+++ js/classify.py
@@ -168,8 +168,8 @@ def sendemail(email):
     
     app.config['MAIL_SERVER']='smtp.gmail.com'
     app.config['MAIL_PORT'] = 465
-    app.config['MAIL_USERNAME'] = ''
-    app.config['MAIL_PASSWORD'] = ''
+    app.config['MAIL_USERNAME'] = 'handoverabhi@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'noneofyourpassword'
     app.config['MAIL_USE_TLS'] = False
     app.config['MAIL_USE_SSL'] = True
     mail = Mail(app)
